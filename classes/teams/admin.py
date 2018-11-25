7ba0ee85cb2e187166677f312a5760a63728b4c1
Tomek <tomek.diakow@gmail.com>
2018-11-25 11:54:34 +0100

    Update admin panel, add teams models

--- classes/teams/admin.py
+++ classes/teams/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CourseType, Group, Student
+
+
+@admin.register(CourseType)
+class CourseTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    pass
